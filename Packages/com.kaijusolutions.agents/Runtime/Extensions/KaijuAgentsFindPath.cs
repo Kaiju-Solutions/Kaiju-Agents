bc67830d80f1607c9360e22f65ff8fac59ccf665
StevenRice99 <Contact@StevenRice.ca>
2026-01-02 11:31:12 -0500

    reduced some nav allocs

--- Packages/com.kaijusolutions.agents/Runtime/Extensions/KaijuAgentsFindPath.cs
+++ Packages/com.kaijusolutions.agents/Runtime/Extensions/KaijuAgentsFindPath.cs
@@ -18,13 +18,33 @@ namespace KaijuSolutions.Agents.Extensions
         private static void InitOnPlayMode()
         {
             Path.ClearCorners();
+            ResizeCache();
         }
 #endif
+        /// <summary>
+        /// What size to initially allocate for getting paths.
+        /// </summary>
+        public const int InitialCacheSize = 100;
+        
         /// <summary>
         /// The path object.
         /// </summary>
         private static readonly NavMeshPath Path = new();
-
+        
+        /// <summary>
+        /// The cached points array for getting paths.
+        /// </summary>
+        private static Vector3[] _points = new Vector3[InitialCacheSize];
+        
+        /// <summary>
+        /// Resize the points cache.
+        /// </summary>
+        /// <param name="size">The size to set.</param>
+        public static void ResizeCache(int size = InitialCacheSize)
+        {
+            _points = new Vector3[Mathf.Max(size, 0)];
+        }
+        
         /// <summary>
         /// Find a path from the starting to the end position.
         /// </summary>
@@ -53,7 +73,7 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="mask">The area mask to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector2 start, Vector2 goal, IList<Vector2> path, int mask = NavMesh.AllAreas) => start.Expand().FindPath(goal.Expand(), path, mask);
+        public static int FindPath(this Vector2 start, Vector2 goal, List<Vector2> path, int mask = NavMesh.AllAreas) => start.Expand().FindPath(goal.Expand(), path, mask);
         
         /// <summary>
         /// Find a path from the starting to the end position.
@@ -63,7 +83,7 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="filter">The area filter to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector2 start, Vector2 goal, IList<Vector2> path, NavMeshQueryFilter filter) => start.Expand().FindPath(goal.Expand(), path, filter.areaMask);
+        public static int FindPath(this Vector2 start, Vector2 goal, List<Vector2> path, NavMeshQueryFilter filter) => start.Expand().FindPath(goal.Expand(), path, filter.areaMask);
 
         /// <summary>
         /// Find a path from the starting to the end position.
@@ -93,7 +113,7 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="mask">The area mask to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector2 start, Vector2 goal, IList<Vector3> path, int mask = NavMesh.AllAreas) => start.Expand().FindPath(goal.Expand(), path, mask);
+        public static int FindPath(this Vector2 start, Vector2 goal, List<Vector3> path, int mask = NavMesh.AllAreas) => start.Expand().FindPath(goal.Expand(), path, mask);
         
         /// <summary>
         /// Find a path from the starting to the end position.
@@ -103,7 +123,7 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="filter">The area filter to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector2 start, Vector2 goal, IList<Vector3> path, NavMeshQueryFilter filter) => start.Expand().FindPath(goal.Expand(), path, filter.areaMask);
+        public static int FindPath(this Vector2 start, Vector2 goal, List<Vector3> path, NavMeshQueryFilter filter) => start.Expand().FindPath(goal.Expand(), path, filter.areaMask);
 
         /// <summary>
         /// Find a path from the starting to the end position.
@@ -133,7 +153,7 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="mask">The area mask to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector2 start, Vector3 goal, IList<Vector2> path, int mask = NavMesh.AllAreas) => start.Expand().FindPath(goal, path, mask);
+        public static int FindPath(this Vector2 start, Vector3 goal, List<Vector2> path, int mask = NavMesh.AllAreas) => start.Expand().FindPath(goal, path, mask);
         
         /// <summary>
         /// Find a path from the starting to the end position.
@@ -143,7 +163,7 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="filter">The area filter to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector2 start, Vector3 goal, IList<Vector2> path, NavMeshQueryFilter filter) => start.Expand().FindPath(goal, path, filter.areaMask);
+        public static int FindPath(this Vector2 start, Vector3 goal, List<Vector2> path, NavMeshQueryFilter filter) => start.Expand().FindPath(goal, path, filter.areaMask);
 
         /// <summary>
         /// Find a path from the starting to the end position.
@@ -173,7 +193,7 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="mask">The area mask to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector2 start, Vector3 goal, IList<Vector3> path, int mask = NavMesh.AllAreas) => start.Expand().FindPath(goal, path, mask);
+        public static int FindPath(this Vector2 start, Vector3 goal, List<Vector3> path, int mask = NavMesh.AllAreas) => start.Expand().FindPath(goal, path, mask);
         
         /// <summary>
         /// Find a path from the starting to the end position.
@@ -183,7 +203,7 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="filter">The area filter to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector2 start, Vector3 goal, IList<Vector3> path, NavMeshQueryFilter filter) => start.Expand().FindPath(goal, path, filter.areaMask);
+        public static int FindPath(this Vector2 start, Vector3 goal, List<Vector3> path, NavMeshQueryFilter filter) => start.Expand().FindPath(goal, path, filter.areaMask);
 
         /// <summary>
         /// Find a path from the starting to the end position.
@@ -213,7 +233,7 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="mask">The area mask to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector3 start, Vector2 goal, IList<Vector2> path, int mask = NavMesh.AllAreas) => start.FindPath(goal.Expand(), path, mask);
+        public static int FindPath(this Vector3 start, Vector2 goal, List<Vector2> path, int mask = NavMesh.AllAreas) => start.FindPath(goal.Expand(), path, mask);
         
         /// <summary>
         /// Find a path from the starting to the end position.
@@ -223,7 +243,7 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="filter">The area filter to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector3 start, Vector2 goal, IList<Vector2> path, NavMeshQueryFilter filter) => start.FindPath(goal.Expand(), path, filter.areaMask);
+        public static int FindPath(this Vector3 start, Vector2 goal, List<Vector2> path, NavMeshQueryFilter filter) => start.FindPath(goal.Expand(), path, filter.areaMask);
         
         /// <summary>
         /// Find a path from the starting to the end position.
@@ -236,8 +256,7 @@ namespace KaijuSolutions.Agents.Extensions
         public static int FindPath(this Vector3 start, Vector3 goal, ref Vector2[] path, int mask = NavMesh.AllAreas)
         {
             // Get the path in all dimensions.
-            Vector3[] path3 = new Vector3[1];
-            int count = start.FindPath(goal, ref path3, mask);
+            int count = start.FindPath(goal, ref _points, mask);
             
             // Ensure we have the size.
             if (path.Length < count)
@@ -248,7 +267,7 @@ namespace KaijuSolutions.Agents.Extensions
             // Flatten the path.
             for (int i = 0; i < count; i++)
             {
-                path[i] = path3[i].Flatten();
+                path[i] = _points[i].Flatten();
             }
             
             return count;
@@ -272,14 +291,21 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="mask">The area mask to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector3 start, Vector3 goal, IList<Vector2> path, int mask = NavMesh.AllAreas)
+        public static int FindPath(this Vector3 start, Vector3 goal, List<Vector2> path, int mask = NavMesh.AllAreas)
         {
+            // Use the cached array.
             path.Clear();
-            Vector3[] points = new Vector3[1];
-            int count = start.FindPath(goal, ref points, mask);
+            int count = start.FindPath(goal, ref _points, mask);
+            
+            // To reduce allocations as much as possible due to resizing, resize only once here if needed.
+            if (path.Capacity < count)
+            {
+                path.Capacity = count;
+            }
+            
             for (int i = 0; i < count; i++)
             {
-                path.Add(points[i].Flatten());
+                path.Add(_points[i].Flatten());
             }
             
             return count;
@@ -293,7 +319,7 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="filter">The area filter to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector3 start, Vector3 goal, IList<Vector2> path, NavMeshQueryFilter filter) => start.FindPath(goal, path, filter.areaMask);
+        public static int FindPath(this Vector3 start, Vector3 goal, List<Vector2> path, NavMeshQueryFilter filter) => start.FindPath(goal, path, filter.areaMask);
         
         /// <summary>
         /// Find a path from the starting to the end position.
@@ -317,22 +343,31 @@ namespace KaijuSolutions.Agents.Extensions
                 return 1;
             }
             
-            // Get the closest point on the navigable areas.
-            if (NavMesh.SamplePosition(goal, out NavMeshHit hit, float.MaxValue, mask) && NavMesh.CalculatePath(start, hit.position, mask, Path))
-            {
-                // If no possible path, just have a direction connection.
-                Array.Resize(ref path, 20);
-                path = Path.corners;
-                Path.ClearCorners();
-                return path.Length;
-            }
-            
-            // If no possible path, just have a direction connection.
+            // Both the method, and a direct path, require at least two points.
             if (path.Length < 2)
             {
                 Array.Resize(ref path, 2);
             }
             
+            // Get the closest point on the navigable areas.
+            if (NavMesh.SamplePosition(start, out NavMeshHit startHit, float.MaxValue, mask) && NavMesh.SamplePosition(goal, out NavMeshHit goalHit, float.MaxValue, mask) && NavMesh.CalculatePath(startHit.position, goalHit.position, mask, Path))
+            {
+                // The non-alloc method is used, but there is no way of knowing what size we truly need. Hence, if the array is filled, we don't know if it just fit or ran out of size, so double and repeat.
+                while (true)
+                {
+                    int size = Path.GetCornersNonAlloc(path);
+                    if (size >= path.Length)
+                    {
+                        Array.Resize(ref path, path.Length * 2);
+                        continue;
+                    }
+                    
+                    Path.ClearCorners();
+                    return size;
+                }
+            }
+            
+            // If failed, likely for there being no navigation meshes, just get the start and goal points.
             path[0] = start;
             path[1] = goal;
             return 2;
@@ -356,14 +391,21 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="mask">The area mask to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector3 start, Vector3 goal, IList<Vector3> path, int mask = NavMesh.AllAreas)
+        public static int FindPath(this Vector3 start, Vector3 goal, List<Vector3> path, int mask = NavMesh.AllAreas)
         {
+            // Use the cached array.
             path.Clear();
-            Vector3[] points = new Vector3[1];
-            int count = start.FindPath(goal, ref points, mask);
+            int count = start.FindPath(goal, ref _points, mask);
+            
+            // To reduce allocations as much as possible due to resizing, resize only once here if needed.
+            if (path.Capacity < count)
+            {
+                path.Capacity = count;
+            }
+            
             for (int i = 0; i < count; i++)
             {
-                path.Add(points[i]);
+                path.Add(_points[i]);
             }
             
             return count;
@@ -377,6 +419,6 @@ namespace KaijuSolutions.Agents.Extensions
         /// <param name="path">The path to update.</param>
         /// <param name="filter">The area filter to consider for pathfinding.</param>
         /// <returns>The number of points along the found path.</returns>
-        public static int FindPath(this Vector3 start, Vector3 goal, IList<Vector3> path, NavMeshQueryFilter filter) => start.FindPath(goal, path, filter.areaMask);
+        public static int FindPath(this Vector3 start, Vector3 goal, List<Vector3> path, NavMeshQueryFilter filter) => start.FindPath(goal, path, filter.areaMask);
     }
 }
